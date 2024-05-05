97a8b8bfa6c48114d4afbca3dd864df4f8de4eb6
sworc <lightbox@nifty.com>
2024-05-05 22:23:55 +0900

    Update menu.php

--- menu.php
+++ menu.php
@@ -14,7 +14,7 @@
     : <a target="_blank" href="https://developer.mozilla.org/ja/docs/Web/HTML/Element/datalist">datalist</a>
 
     
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+
   </body>
 </html>
 
